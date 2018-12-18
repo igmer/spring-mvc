690cf552aede61d044cf5145d6c3a16f23abc87b
rodriguez <igmerdz@gmail.com>
2018-12-18 08:36:53 -0600

    inicio de proyecto spring

---
+++ src/main/java/com/igmer/cineapp/App.java
@@ -0,0 +1,13 @@
+package com.igmer.cineapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
